9c40cd963a21f25fcd5433c58423fa2bd85e4945
kajlasanamdeep <kajlasanamdeep@gmail.com>
2022-09-21 13:09:13 +0530

    update code file

--- index.js
+++ index.js
@@ -6,14 +6,13 @@ const express = require('express');
 const app = express();
 const controllers = require('./controllers');
 const middlewares = require('./middlewares');
-const { Console } = require('console');
 
 // initalizing Apis
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use('/image', express.static(path.join(__dirname, './build')));
 app.post('/NFT', middlewares.uploadFiles,controllers.createNft);
-console.log('test')
+console.log('test123')
 // starting Server
 app.listen(8000, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${8000}`);
