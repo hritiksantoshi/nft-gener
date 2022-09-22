c953bdba870f04fa467986c49ce2e4fea6f4952c
kajlasanamdeep <kajlasanamdeep@gmail.com>
2022-09-22 18:52:18 +0530

    User register&Login APIs

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const Controllers = require('../Controllers');
-const Middlewares = require('../Middlewares');
+const Controllers = require('../controllers');
+const Middlewares = require('../middlewares');
 router.post('/register',Controllers.UserController.createUser);
 router.post('/login',Controllers.UserController.login);
 router.post('/generateNFT', Middlewares.UploadFiles,Controllers.UserController.createNft);
