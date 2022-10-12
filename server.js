e3eeb6c4f49d9984b8a97ce34f989b52f2ed57b3
kajlasanamdeep <kajlasanamdeep@gmail.com>
2022-10-12 15:19:13 +0530

    cors error solved!

--- server.js
+++ server.js
@@ -14,22 +14,22 @@ const swaggerJson = require('./swagger.json');
 const origins = [
     'http://localhost:3000'
 ]
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        if (origins.indexOf(origin) === -1) {
-            let msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
-    optionsSuccessStatus: 200
-};
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//         if (!origin) return callback(null, true);
+//         if (origins.indexOf(origin) === -1) {
+//             let msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+//             return callback(new Error(msg), false);
+//         }
+//         return callback(null, true);
+//     },
+//     optionsSuccessStatus: 200
+// };
 
 // initalizing Apis
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors(corsOptions));
+app.use(cors());
 app.use('/Images', express.static(path.join(__dirname, './Uploads')));
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerJson));
 app.use('/api', Routes);
